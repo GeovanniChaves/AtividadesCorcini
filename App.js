fffe78c1ad7dba0335f389c6697e190ce353f7ec
GeovanniChaves <chavesgeovanni0@gmail.com>
2026-03-06 17:55:05 -0300

    Atividade 9

--- App.js
+++ App.js
@@ -5,15 +5,20 @@ import { View, Text } from 'react-native';
 
 function App() {
   return (
-    <View style={{ flex: 1, marginBottom: 0, backgroundColor: "orange", flexDirection: 'column',
-      justifyContent: 'center' }}>
-      <View style={{ width: 70, height: 70, backgroundColor: 'powderblue' }}>
+    <View style={{ 
+      flex: 1, 
+      padding: 20, 
+      backgroundColor: "white", 
+      flexDirection: 'column',
+      justifyContent: 'space-around' 
+    }}>
+      <View style={{ width: 70, height: 70, backgroundColor: 'green' }}>
       </View>
       
-      <View style={{ width: 70, height: 70, backgroundColor: 'skyblue' }}>
+      <View style={{ width: 70, height: 70, backgroundColor: 'red' }}>
       </View>
       
-      <View style={{ width: 70, height: 70, backgroundColor: 'steelblue' }}>
+      <View style={{ width: 70, height: 70, backgroundColor: 'black' }}>
       </View>
     </View>
   );
